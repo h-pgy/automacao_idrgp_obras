60301a696b719998abacc2fe57ab20411ff4797c
hpgy <hpougy@gmail.com>
2024-03-12 17:27:14 -0300

    inicio do ETL

---
+++ config.py
@@ -0,0 +1,39 @@
+import os
+from typing import Union
+from dotenv import load_dotenv
+
+def copy_dot_env_example():
+
+    if not os.path.exists('.env'):
+        print('Definindo o ambiente a partir da cópia do arquivo .env.example.')
+        with open('.env.example', 'r') as example:
+            with open('.env', 'w') as env_file:
+                env_file.write(example.read())
+
+
+
+def load_env():
+
+    copy_dot_env_example()
+    load_dotenv()
+
+def load_env_var(varname:str)->Union[int, float, str]:
+
+    try:
+        return os.environ[varname]
+    except KeyError:
+        raise RuntimeError(f'Variavel de ambiente {varname} não definida.')
+    
+def solve_dir(dirname:str)->str:
+
+    if not os.path.exists(dirname):
+        os.mkdir(dirname)
+    return dirname
+    
+
+load_env()
+
+ORIGINAL_DATA_DIR=solve_dir(load_env_var('ORIGINAL_DATA_DIR'))
+GENERATED_DATA_DIR=solve_dir(load_env_var('GENERATED_DATA_DIR'))
+
+SOF_API_TOKEN=load_env_var('SOF_API_TOKEN')
